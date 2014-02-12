185f03dad7fd8a15efd9d40c52e9328de5770a90
Giacomo <giacomolm@hotmail.it>
2014-02-12 10:31:27 +0100

    verifying size

--- core/src/main/java/org/apache/syncope/core/init/CamelRouteLoader.java
+++ core/src/main/java/org/apache/syncope/core/init/CamelRouteLoader.java
@@ -62,7 +62,7 @@ public class CamelRouteLoader {
         
         try{
             ApplicationContext context = ApplicationContextProvider.getApplicationContext();
-            LOG.info("Ecco il contesto {}", context.getBean("camel-context", DefaultCamelContext.class));            
+            LOG.info("Ecco il contestooo size {}", context.getBean("camel-context", DefaultCamelContext.class).getRoutes().size());            
         }catch(Exception e){
             LOG.info("fallimento nel contesto {}", e);
         }
